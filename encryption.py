ae9bb14c4ef6e0aa769ceaa1e2409317e26fe7e5
Yohann Pirbay <pirbay.yohann@gmail.com>
2024-08-14 15:34:03 +0400

    add encryption logic

---
+++ encryption.py
@@ -0,0 +1,35 @@
+from cryptography.fernet import Fernet
+import os
+import time
+
+# Generates a key for encryption
+def generate_key():
+    return(Fernet.generate_key())
+
+# Encrypts a given file with the user's gien key
+def encrypt_file(input_file, key):
+    fernet = Fernet(key)
+
+    with open(input_file, 'rb') as file:
+        plain_text = file.read()
+
+    os.remove(input_file)
+
+    with open(input_file, 'wb') as file:
+        cipher_text = fernet.encrypt(plain_text)
+        file.write(cipher_text)
+
+# Decrypts a given file with the user's gien key
+def decrypt_file(input_file, key):
+    fernet = Fernet(key)
+
+    with open(input_file, 'rb') as file:
+        cipher_text = file.read()
+
+        os.remove(input_file)
+
+    with open(input_file, 'wb') as file:
+        plain_text = fernet.decrypt(cipher_text)
+        file.write(plain_text)
+
+
